7b29224706dd46ceab6a808178c614d186d35bd3
Just-Mario <mario.nyangintaka@gmail.com>
2021-01-16 15:45:09 +0200

    added features

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import { Card, Row, Col, Image, Button, ListGroup } from 'react-bootstrap';
 import Rating from '../components/Rating';
 import products from '../products';
 
@@ -12,6 +12,7 @@ const ProductScreen = ({ match }) => {
 			<Link className='btn btn-light my-3' to='/'>
 				Go back
 			</Link>
+
 			<Row>
 				<Col md={6}>
 					<Image src={product.image} alt={product.name} fluid />
@@ -21,16 +22,14 @@ const ProductScreen = ({ match }) => {
 						<ListGroup.Item>
 							<h3>{product.name}</h3>
 						</ListGroup.Item>
-
 						<ListGroup.Item>
 							<Rating
 								value={product.rating}
 								text={`${product.numReviews} reviews`}
 							/>
 						</ListGroup.Item>
-
 						<ListGroup.Item>Price: ${product.price}</ListGroup.Item>
-						<ListGroup.Item>Description: ${product.description}</ListGroup.Item>
+						<ListGroup.Item>Description: {product.description}</ListGroup.Item>
 					</ListGroup>
 				</Col>
 				<Col md={3}>
@@ -40,7 +39,7 @@ const ProductScreen = ({ match }) => {
 								<Row>
 									<Col>Price:</Col>
 									<Col>
-										<strong>${product.price}</strong>
+										<strong>${`${product.price}`}</strong>
 									</Col>
 								</Row>
 							</ListGroup.Item>
@@ -48,11 +47,12 @@ const ProductScreen = ({ match }) => {
 								<Row>
 									<Col>Status:</Col>
 									<Col>
-										{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+										<strong>
+											{product.countInStock > 0 ? 'In Stock' : 'Out of stock'}
+										</strong>
 									</Col>
 								</Row>
 							</ListGroup.Item>
-
 							<ListGroup.Item>
 								<Button
 									className='btn-block'
