917259b5a7f71326be0322042245aba37ce7f046
Just-Mario <mario.nyangintaka@gmail.com>
2021-01-16 20:47:59 +0200

    Added backend functionality

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -16,7 +16,7 @@ const Header = () => {
 						<Nav className='ml-auto'>
 							<LinkContainer to='/cart'>
 								<Nav.Link>
-									<i className='fas fa-shopping-cart'></i> Cart
+									<i className='fas fa-shopping-cart'></i>Cart
 								</Nav.Link>
 							</LinkContainer>
 							<LinkContainer to='/login'>
