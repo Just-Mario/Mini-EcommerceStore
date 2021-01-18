b1e00069658e93b89ccd2a891fc46d1c73d0aa4f
Just-Mario <mario.nyangintaka@gmail.com>
2021-01-18 20:39:00 +0200

    updated backend

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,47 @@
+//Initialise Express.
 import express from 'express';
-import dotenv from 'dotenv';
+
+//Requesting products from backend.
 import products from './data/products.js';
 
-dotenv.config();
+//Requesting dotenv
+import dotenv from 'dotenv';
+//Requesting colors for Terminal color coding.
+import colors from 'colors';
+//Requesting ConnectDB from DB.JS.
+import connectDB from './config/db.js';
 
+dotenv.config();
+connectDB();
 const app = express();
 
+//Test
 app.get('/', (req, res) => {
-	res.send('API is running...');
+	app.res('API is running...');
+});
+//Rauting Testing
+app.get('/', (req, res) => {
+	app.res('API is running...');
 });
 
+//Rauting All products render on client Browser.
 app.get('/api/products', (req, res) => {
-	res.json(products);
+	app.res(products);
 });
 
+//Rauting -Iterate products.js Objects Erray & render each one on client Browser.
 app.get('/api/products/:id', (req, res) => {
 	const product = products.find((p) => p._id === req.params.id);
-	res.json(product);
+	app.res(product);
 });
 
+//Application confirmation rendering.
 const PORT = process.env.PORT || 5000;
 
 app.listen(
 	PORT,
-	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+	console.log(
+		`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.cyan
+			.bold.underline
+	)
 );
